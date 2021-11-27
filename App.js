210d23127f27e407ec201fc5dcdbb9262db70a55
sAmH365 <npower5377@naver.com>
2021-11-27 15:12:24 +0900

    Style

--- App.js
+++ App.js
@@ -7,16 +7,54 @@
  */
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 class App extends Component {
   render() {
     return ( 
-      <View>
-        <Text>react Native</Text>
+      <View style={styles.mainView}>
+        <View style={styles.subView}>
+          <Text style={styles.mainText}>react Native</Text>
+        </View>
+        <View style={styles.subView}>
+          <Text>react Native</Text>
+        </View>
+        <View style={styles.anotherSubView}>
+          <Text style={styles.mainText}>react Native</Text>
+        </View>
       </View>
   )
   }
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    backgroundColor: 'green',
+    paddingTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  subView: {
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    flex: 1,
+    width: '50%'
+  },
+  anotherSubView: {
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 2
+  },
+  mainText: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 50,
+    padding: 20,
+  }
+})
 
 export default App;
